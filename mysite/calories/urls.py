5e8c7b293eeaa3e75ae340e64897244ae9b5f27c
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-06 11:21:54 +0300

    Rename view for start page

--- mysite/calories/urls.py
+++ mysite/calories/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.start_page, name='start_page'),
 ]
--- mysite/calories/views.py
+++ mysite/calories/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def start_page(request):
     return HttpResponse("Hello world. You're at the calories control service")
