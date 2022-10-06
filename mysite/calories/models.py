2ef11125f51c761a06bb1310b1e14452220cc1b4
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-06 13:19:08 +0300

    Create `Food` and `EatenFood` models

--- mysite/calories/models.py
+++ mysite/calories/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=200)
+    #measured in calories per 100 grams
+    calories = models.FloatField()
+    user_added = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class EatenFood(models.Model):
+    name = models.ForeignKey(Food, on_delete=models.CASCADE)
+    user_eaten = models.ForeignKey(User, on_delete=models.CASCADE)
+    #measured in grams
+    weight_eaten = models.FloatField()
+    eating_time = models.DateTimeField()
+
