63e0ab696fc315cf26a7fa26ba1dd8ab597b4808
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-07 15:42:09 +0300

    Refactoring calories/models.py

--- mysite/calories/models.py
+++ mysite/calories/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Food(models.Model):
-    name = models.CharField(max_length=200)
+    food = models.CharField(max_length=200)
     #measured in calories per 100 grams
     calories = models.FloatField()
     user_added = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.food
 
 class EatenFood(models.Model):
-    name = models.ForeignKey(Food, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
     user_eaten = models.ForeignKey(User, on_delete=models.CASCADE)
     #measured in grams
     weight_eaten = models.FloatField()
     eating_time = models.DateTimeField()
 
     def __str__(self):
-        return self.name
+        return self.food
 
