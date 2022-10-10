c4b958b7016505d69908e22ec85c1d8750f948bb
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-10 16:40:15 +0300

    Update home_page view

--- mysite/calories/views.py
+++ mysite/calories/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Food
 
 def home_page(request):
-    return HttpResponse("Hello world. You're at the calories control service")
+    food_list = Food.objects.order_by('name')
+    return render(request, 'calories/home_page.html', {'food_list': food_list})
