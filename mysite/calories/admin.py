2840187b64658380604f505041c5c730b74f68cd
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-10 18:12:46 +0300

    Add FoodCategory model. Add category field to Food model. Make migrations for changes in models.

--- mysite/calories/admin.py
+++ mysite/calories/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Food, EatenFood
+from .models import Food, EatenFood, FoodCategory
 
 admin.site.register(Food)
 admin.site.register(EatenFood)
+admin.site.register(FoodCategory)
--- mysite/calories/models.py
+++ mysite/calories/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class FoodCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Food(models.Model):
     name = models.CharField(max_length=200)
     #measured in calories per 100 grams
     calories = models.FloatField()
     user_added = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(FoodCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
 
 class EatenFood(models.Model):
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
