9e299bb113224b0f13c0226d2bf75549beed5dcb
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-11 14:25:01 +0300

    Add __str__ method to FoodCategory model

--- mysite/calories/models.py
+++ mysite/calories/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class FoodCategory(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 
 class Food(models.Model):
