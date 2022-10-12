c11de90e5569157c59e3877aa3e2a0006377d589
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-12 13:44:36 +0300

    Refactoring home_page view and home_page.html

--- mysite/calories/views.py
+++ mysite/calories/views.py
@@ -4,5 +4,6 @@ from .models import FoodCategory, Food
 def home_page(request):
     category_food = {}
     for category in FoodCategory.objects.all():
-        category_food[category] =  Food.objects.filter(category=category).order_by('name')
+        if food := Food.objects.filter(category=category).order_by('name'):
+            category_food[category] = food
     return render(request, 'calories/home_page.html', {'category_food': category_food})
