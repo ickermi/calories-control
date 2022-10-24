3e1983c3d3249bc9fa70b6e8efb714603a3a42d6
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-24 17:26:48 +0300

    Add the ability to log out

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', views.LoginView.as_view(template_name = 'login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name = 'logged_out.html'), name='logout'),
 ]
