93b61b6f0ca0424f441ebf34bf025afbf351200c
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-31 18:51:50 +0300

    Return login template

--- mysite/calories/urls.py
+++ mysite/calories/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('registration/', views.registration, name='registration'),
 ]
