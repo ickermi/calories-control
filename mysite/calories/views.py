1109b65d2f3deb721d658beb8bc66352c6e90aff
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-31 14:14:21 +0300

    Add view for registration form

--- mysite/calories/views.py
+++ mysite/calories/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse, redirect, HttpResponse
 from .models import FoodCategory, Food
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegistrationForm
 
 
 def home_page(request):
@@ -22,3 +23,17 @@ def home_page(request):
         'form': form,
     }
     return render(request, 'home_page.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return redirect(reverse('home_page'))
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'registragion.html', {'form': form})
