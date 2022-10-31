882702a94f44d35f216e0e7d9ab179d8b488c95e
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-31 12:34:43 +0300

    Rename password validation func. Now runs when `is_valid` method is called

--- mysite/calories/forms.py
+++ mysite/calories/forms.py
@@ -2,15 +2,15 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='password', widget=forms.PasswordInput)
-    repeated_password = forms.CharField(label='password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'email']
+        fields = ('username', 'first_name', 'email')
 
-    def check_password_match(self):
+    def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['repeated_password']:
-            raise forms.ValidationError('Пароли не совпадают')
-        return cd['password']
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
