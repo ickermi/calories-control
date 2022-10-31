589547aed8c3eb3d1711e5a48cf2793184048464
Dmitrii Ostapenko <MikeIkerson@yandex.ru>
2022-10-31 18:35:38 +0300

    Remove login and logout templates as unnecessary

--- mysite/calories/urls.py
+++ mysite/calories/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'logged_out.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('registration/', views.registration, name='registration'),
 ]
